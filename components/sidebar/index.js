245e37c2c7a6b84559ec8dbe56817778f025589f
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-07-07 11:45:33 +0200

    Implement authenticator function, login/logout

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -2,6 +2,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import styles from "./sidebar.module.css";
 import classNames from "classnames";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const navItems = {
   "/": {
@@ -22,18 +23,67 @@ const navItems = {
 };
 
 export default function Navbar() {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+
   let pathname = usePathname() || "/";
   if (pathname.includes("/blog/")) {
     pathname = "/blog";
   }
+
+  const navItems = {
+    "/": {
+      name: "Home",
+    },
+    "/about": {
+      name: "About",
+    },
+    "/blog": {
+      name: "Blog",
+    },
+    "/create-post": {
+      name: "Create post",
+      requiresAuth: true,
+    },
+    "/login": {
+      name: "Login",
+      requiresAuth: false,
+    },
+    "/logout": {
+      name: "Logout",
+      requiresAuth: true,
+      onClick: async() => {
+        await supabaseClient.auth.signOut();
+        console.log("Clicked");
+      },
+    },
+  };
 
   return (
     <aside className={styles.container}>
       <div className={styles.sticky}>
         <nav className={styles.navigation} id="nav">
           <div className={styles.navigationItemWrapper}>
-            {Object.entries(navItems).map(([path, { name }]) => {
+            {Object.entries(navItems).map(([path, { name, requiresAuth, onClick }]) => {
               const isActive = path === pathname;
+
+              if((requiresAuth && !user) || (path === "/login" && user)) {
+                return null;
+              }
+
+              if (path === "/logout") {
+                return <button
+                  className={classNames(styles.navigationButton, {
+                    [styles.textNeutral]: !isActive,
+                    [styles.fontBold]: isActive,
+                  })}
+                  key={path}
+                  onClick={onClick}
+                >
+                  {name}
+                </button>;
+              }
+
               return (
                 <Link
                   key={path}
