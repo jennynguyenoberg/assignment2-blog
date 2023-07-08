d55ea057b7696ad8395b373495fbf07f341df74f
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-07-08 17:09:37 +0200

    Add random text to About-page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,5 +1,10 @@
 import Heading from "@components/heading";
 
 export default function About() {
-  return <Heading>About</Heading>;
+  return (
+    <div>
+      <Heading>About</Heading>
+      <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident maxime labore ab voluptas sequi ratione quasi nulla fugit, vel eligendi facilis voluptatem cupiditate, maiores asperiores, eaque dolorem unde deserunt.</p>
+    </div> 
+  )
 }
