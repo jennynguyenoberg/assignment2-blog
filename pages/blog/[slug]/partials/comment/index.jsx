5850067a8e075cb22fbde1382cc8db87cff43beb
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-07-08 16:13:51 +0200

    Add reset function to comment deletion

--- pages/blog/[slug]/partials/comment/index.jsx
+++ pages/blog/[slug]/partials/comment/index.jsx
@@ -1,10 +1,12 @@
 import Button from "@components/button";
 import styles from "./comment.module.css";
+import { useRef } from "react";
 
 import { removeComment, commentsCacheKey } from "@/api-routes/comments";
 import useSWRMutation from "swr/mutation";
 
 export default function Comment({ comment, created_at, author, id }) {
+  const formRef = useRef();
   const { trigger: removeCommentTrigger } = useSWRMutation(commentsCacheKey,
     removeComment,
   );
@@ -15,8 +17,8 @@ export default function Comment({ comment, created_at, author, id }) {
     if (error) {
       console.log(error);
     }
+    formRef.current.reset();
   };
-  console.log({ id });
   
   return (
     <div className={styles.container}>
@@ -25,9 +27,11 @@ export default function Comment({ comment, created_at, author, id }) {
       <time className={styles.date}>{created_at}</time>
 
       {/* The Delete part should only be showed if you are authenticated and you are the author */}
-      <div className={styles.buttonContainer}>
-        <Button onClick={handleDelete}>Delete</Button>
-      </div>
+      <form ref={formRef} className={styles.form}>
+        <div className={styles.buttonContainer}>
+          <Button onClick={handleDelete}>Delete</Button>
+        </div>
+      </form>
     </div>
   );
 }
