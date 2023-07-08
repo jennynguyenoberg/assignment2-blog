02fd284136f0dd5e6933a0f6c9007d537b115cfc
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-07-08 15:22:31 +0200

    Add comment delete function

--- pages/blog/[slug]/partials/comment/index.jsx
+++ pages/blog/[slug]/partials/comment/index.jsx
@@ -1,15 +1,28 @@
 import Button from "@components/button";
 import styles from "./comment.module.css";
 
-export default function Comment({ comment, createdAt, author, id }) {
-  const handleDelete = () => {
-    console.log({ id });
+import { removeComment, commentsCacheKey } from "@/api-routes/comments";
+import useSWRMutation from "swr/mutation";
+
+export default function Comment({ comment, created_at, author, id }) {
+  const { trigger: removeCommentTrigger } = useSWRMutation(commentsCacheKey,
+    removeComment,
+  );
+
+  const handleDelete = async () => {
+    const { error } = await removeCommentTrigger(id);
+    
+    if (error) {
+      console.log(error);
+    }
   };
+  console.log({ id });
+  
   return (
     <div className={styles.container}>
       <p>{comment}</p>
       <p className={styles.author}>{author}</p>
-      <time className={styles.date}>{createdAt}</time>
+      <time className={styles.date}>{created_at}</time>
 
       {/* The Delete part should only be showed if you are authenticated and you are the author */}
       <div className={styles.buttonContainer}>
