b33f2ee90c6cfe5c11bac0b9c81eda1b7c70d348
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-02 09:45:46 +0200

    Add searchbar that filters through post titles

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -4,13 +4,26 @@ import Heading from '@components/heading';
 
 import useSWR from 'swr';
 import { getPosts, postsCacheKey } from '../../api-routes/posts';
+import { useState } from 'react';
 
 export default function Blog() {
   const { data: { data = [] } = {} } = useSWR(postsCacheKey, getPosts);
+  
+  const [searchQuery, setSearchQuery] = useState('');
+  const filteredData = data.filter((post) =>
+    post.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <section>
       <Heading>All Articles</Heading>
-      {data?.map((post) => (
+      <input
+        type="text"
+        placeholder="Search by title"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      {filteredData.map((post) => (
         <Link
           key={post.slug}
           className={styles.link}
