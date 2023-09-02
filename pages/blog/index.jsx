d00fcf077cf5aec59798b97f5d3675664eaae45c
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-02 11:31:30 +0200

    Clean up and add new cleaner logic

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,36 +1,29 @@
-import Link from 'next/link';
-import styles from './blog.module.css';
-import Heading from '@components/heading';
-
-import useSWR from 'swr';
-import { getPosts, postsCacheKey } from '../../api-routes/posts';
 import { useState } from 'react';
+import { getPosts, postsCacheKey } from '../../api-routes/posts';
+import useSWR from 'swr';
+import Heading from '@components/heading';
+import styles from './blog.module.css';
+import Link from 'next/link';
 
 export default function Blog() {
   const { data, error } = useSWR(postsCacheKey, getPosts);
-
   const [searchQuery, setSearchQuery] = useState('');
   const [showAll, setShowAll] = useState(false);
+  const filteredData = data?.data || [];
+
+  // Reverse the filteredData array
+  const reversedData = [...filteredData].reverse();
+
+  // Determine how many posts to display
+  const displayData = showAll ? reversedData : reversedData.slice(0, 6);
 
   if (error) {
-    return <p>Error loading data: {error.message}</p>;
+    return <div>Error loading data. Please try again later.</div>;
   }
 
   if (!data) {
-    return <p>Loading...</p>;
+    return <div>Loading...</div>;
   }
-
-  const { data: postData = [] } = data;
-
-  const filteredData = postData
-    .filter((post) =>
-      post.title.toLowerCase().includes(searchQuery.toLowerCase())
-    )
-    .slice(0, showAll ? postData.length : 6);
-
-  const toggleShowAll = () => {
-    setShowAll(!showAll);
-  };
 
   return (
     <section>
@@ -41,26 +34,26 @@ export default function Blog() {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      {filteredData.length === 0 ? (
-        <p>No posts found.</p>
-      ) : (
-        filteredData.map((post) => (
-          <Link
-            key={post.slug}
-            className={styles.link}
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col">
-              <p>{post.title}</p>
-              <time className={styles.date}>{post.created_at}</time>
-            </div>
-          </Link>
-        ))
-      )}
-      {postData.length > 6 && (
-        <button onClick={toggleShowAll} className={styles.seeMore}>
-          {showAll ? 'Show Less' : 'See More'}
-        </button>
+      {displayData.map((post) => (
+        <Link
+          key={post.slug}
+          className={styles.link}
+          href={`/blog/${post.slug}`}
+        >
+          <div className="w-full flex flex-col">
+            <p>{post.title}</p>
+            <time className={styles.date}>{post.created_at}</time>
+          </div>
+        </Link>
+      ))}
+      {reversedData.length > 6 && (
+        <div>
+          {showAll ? (
+            <button onClick={() => setShowAll(false)}>Show Less</button>
+          ) : (
+            <button onClick={() => setShowAll(true)}>Show More</button>
+          )}
+        </div>
       )}
     </section>
   );
