6da92e923e737387576116454a3dc8f5daede4d6
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-04 23:56:18 +0200

    Add metadata/title to boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,17 @@ import RootLayout from '../components/root-layout';
 
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { supabase } from '@/lib/supabaseClient';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Jenny Nguyen Öberg&apos;s Blog</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       <SessionContextProvider
         supabaseClient={supabase}
         initialSession={pageProps.initialSession}
