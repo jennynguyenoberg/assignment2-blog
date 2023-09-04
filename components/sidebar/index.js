2a0be2885d5f79514d3a72fdd5789c6a82c242d6
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-04 22:26:02 +0200

    New logic to navbar with styling

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -4,9 +4,12 @@ import styles from './sidebar.module.css';
 import classNames from 'classnames';
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/router';
-import Image from 'next/image'; // Import the Image component from 'next/image'
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
-export default function Navbar() {
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [shouldCloseMenu, setShouldCloseMenu] = useState(false); // Added state variable
   const router = useRouter();
   const supabaseClient = useSupabaseClient();
   const user = useUser();
@@ -18,7 +21,7 @@ export default function Navbar() {
 
   const navItems = {
     '/': {
-      img: '/logo.png',
+      img: '/logo.svg',
     },
     '/about': {
       name: 'About',
@@ -44,62 +47,128 @@ export default function Navbar() {
     },
   };
 
+  const toggleMenu = () => {
+    if (isOpen) {
+      setIsOpen(false);
+      setShouldCloseMenu(false);
+    } else {
+      setIsOpen(true);
+      setShouldCloseMenu(true);
+    }
+  };
+
+  useEffect(() => {
+    const handleDocumentClick = (e) => {
+      const menu = document.querySelector(`.${styles['menu-icon']}`);
+      if (menu && !menu.contains(e.target)) {
+        setIsOpen(false);
+        setShouldCloseMenu(false);
+      }
+    };
+
+    const handleScroll = () => {
+      if (shouldCloseMenu) {
+        setIsOpen(false);
+        setShouldCloseMenu(false);
+      }
+    };
+
+    document.addEventListener('click', handleDocumentClick);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [shouldCloseMenu]); 
+
   return (
-    <aside className={styles.container}>
-      <div className={styles.sticky}>
-        <nav className={styles.navigation} id="nav">
-          <div className={styles.navigationItemWrapper}>
-            {Object.entries(navItems).map(
-              ([path, { img, name, requiresAuth, onClick }]) => {
-                const isActive = path === pathname;
-
-                // Don't render a link that requires auth if there's no user.
-                if ((requiresAuth && !user) || (path === '/login' && user)) {
-                  return null;
-                }
-
-                if (path === '/logout') {
-                  return (
-                    <button
-                      className={classNames(styles.navigationButton, {
-                        [styles.textNeutral]: !isActive,
-                        [styles.fontBold]: isActive,
-                      })}
-                      key={path}
-                      onClick={onClick}
-                    >
-                      {name}
-                    </button>
-                  );
-                }
-
-                return (
-                  <Link
-                    key={path}
-                    href={path}
-                    className={classNames(styles.navigationItem, {
-                      [styles.textNeutral]: !isActive,
-                      [styles.fontBold]: isActive,
-                    })}
-                  >
-                    <span className={styles.linkName}>
-                      {img && (
-                        <Image
-                          src={img}
-                          alt={name}
-                          width={32}
-                          height={32}
-                        />
-                      )}
-                      {name}
-                    </span>
-                  </Link>
-                );
-              },
-            )}
-          </div>
-        </nav>
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <Image src={navItems['/'].img} alt="Logo" width={32} height={32} />
       </div>
-    </aside>
+      <div className={styles.menuContainer}>
+        <div className={`${styles['menu-icon']} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+        <div className={styles.navItemContainer}>
+          {isOpen && (
+            <ul className={`${styles['navbar-items']} ${isOpen ? styles.open : ''}`}>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Login</a>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 }
+
+//   return (
+//     <aside className={styles.container}>
+//       <div className={styles.sticky}>
+//         <nav className={styles.navigation} id="nav">
+//           <div className={styles.navigationItemWrapper}>
+//             {Object.entries(navItems).map(
+//               ([path, { img, name, requiresAuth, onClick }]) => {
+//                 const isActive = path === pathname;
+
+//                 // Don't render a link that requires auth if there's no user.
+//                 if ((requiresAuth && !user) || (path === '/login' && user)) {
+//                   return null;
+//                 }
+
+//                 if (path === '/logout') {
+//                   return (
+//                     <button
+//                       className={classNames(styles.navigationButton, {
+//                         [styles.textNeutral]: !isActive,
+//                         [styles.fontBold]: isActive,
+//                       })}
+//                       key={path}
+//                       onClick={onClick}
+//                     >
+//                       {name}
+//                     </button>
+//                   );
+//                 }
+
+//                 return (
+//                   <Link
+//                     key={path}
+//                     href={path}
+//                     className={classNames(styles.navigationItem, {
+//                       [styles.textNeutral]: !isActive,
+//                       [styles.fontBold]: isActive,
+//                     })}
+//                   >
+//                     <span className={styles.linkName}>
+//                       {img && (
+//                         <Image
+//                           src={img}
+//                           alt={name}
+//                           width={32}
+//                           height={32}
+//                         />
+//                       )}
+//                       {name}
+//                     </span>
+//                   </Link>
+//                 );
+//               },
+//             )}
+//           </div>
+//         </nav>
+//       </div>
+//     </aside>
+//   );
+// }
