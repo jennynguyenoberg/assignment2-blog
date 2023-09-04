aa7bc82d6eb548a3b655528044ed7b4635d97bed
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-04 13:50:06 +0200

    Add loading data to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import Heading from '@components/heading';
 import RecentPosts from '@/pages/recent-posts';
+import { Puff } from 'react-loader-spinner';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true); 
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <Heading>Home</Heading>
-      <RecentPosts />
+      {loading ? ( // Render the loader while data is loading
+        <Puff
+          height="80"
+          width="80"
+          radius={1}
+          color="#cecece"
+          ariaLabel="puff-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+      />
+      ) : (
+        <RecentPosts />
+      )}
     </>
   );
 }
